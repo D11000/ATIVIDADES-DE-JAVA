21fa88f1b2b28574ebf10da17c64024637105dbf
D11000 <118454742+D11000@users.noreply.github.com>
2025-05-13 08:09:05 -0300

    N°1

---
+++ TESTE.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TESTE {
+    public static void main(String[] args) {
+int a, b,c,d;
+        Scanner dados = new Scanner(System.in);
+        System.out.println("Digite o  valor ");
+
+        a= dados.nextInt();
+        System.out.println("Digite o  valor ");
+        b= dados.nextInt();
+        System.out.println("Digite o valor ");
+        c= dados.nextInt();
+        d=a+b+c/3;
+        System.out.println("a media é "+d);
+
+
+    }
+
+
+
+
+
+
+
+
+}
