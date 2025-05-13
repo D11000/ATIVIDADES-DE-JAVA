3d024b9f350265a84b8460e5866b6f1dcb7e8376
D11000 <118454742+D11000@users.noreply.github.com>
2025-05-13 08:22:16 -0300

    N°2

---
+++ Sucessor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Sucessor {
+    public static void main(String[] args) {
+        int a,b,c ;
+        Scanner dados= new Scanner(System.in);
+        System.out.println("Digite o  valor ");
+        a= dados.nextInt();
+        c= a+1;
+        b= a-1;
+        System.out.println("O antecessor de " +a+  "é" +b+ " e o sucessor é " +c);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
